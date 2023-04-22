0730c5c9cbce34758c6293ecf2ae4ecc90e14ad7
Patrick Son <pmacedo10_@hotmail.com>
2023-04-22 13:05:42 -0300

    Adicionada pasta de logs na branch

--- config/configs.js
+++ config/configs.js
@@ -2,18 +2,19 @@ require("dotenv").config();
 
 const database = {
   producao: {
+    host: process.env.DB_HOST_PROD,
+    user: process.env.DB_USER_PROD,
+    pass: process.env.DB_PASS_PROD,
+    base: process.env.DB_BASE_PROD,
+    port: process.env.DB_PORT_PROD
+  },
+
+  desenvolvimento: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     pass: process.env.DB_PASS,
     base: process.env.DB_BASE,
     port: process.env.DB_PORT
-  },
-
-  desenvolvimento: {
-    host: "localhost",
-    user: "root",
-    pass: "",
-    base: "iboardjs",
   },
 
   listPerPage: 10,
