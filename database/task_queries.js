dc841dafb265a707a6ba3cbe337fff68cc582b5f
Patrick Son <pmacedo10_@hotmail.com>
2023-04-27 17:31:23 -0300

    Refatorado

--- database/task_queries.js
+++ database/task_queries.js
@@ -16,14 +16,14 @@ const listarTotalTarefas = `SELECT ABS((SELECT COUNT(*) FROM tasks) / 10) + 1  a
 function listarTarefasPaginadas(page = 0) {
   let offset = page > 1 ? (page - 1) * 10 : 0;
   return `SELECT t.task_id, t.task_user_id, u.user_fullname, t.task_title, t.task_description, p.priority_description, tp.type_description, s.status_description, t.created_at
-    FROM tasks t
-        inner join users u ON u.user_id = t.task_user_id
-        inner join priorities p ON p.priority_id = t.task_priority_id
-        inner join status s ON s.status_id = t.task_status_id
-        inner join types tp ON tp.type_id = t.task_type_id
-    WHERE t.task_status_id !=2
-    ORDER BY t.created_at DESC 
-    LIMIT 10 OFFSET ${offset}`;
+          FROM tasks t
+              inner join users u ON u.user_id = t.task_user_id
+              inner join priorities p ON p.priority_id = t.task_priority_id
+              inner join status s ON s.status_id = t.task_status_id
+              inner join types tp ON tp.type_id = t.task_type_id
+          WHERE s.status_id !=2
+          ORDER BY t.task_id DESC 
+          LIMIT 10 OFFSET ${offset}`;
 }
 
 function cadastrarTarefa(id, model) {
