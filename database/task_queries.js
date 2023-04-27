a00029a96432b7b5eccba5659cee943306aa0808
Patrick Son <pmacedo10_@hotmail.com>
2023-04-27 10:09:36 -0300

    Corrigida a ordem dos resultados de consulta

--- database/task_queries.js
+++ database/task_queries.js
@@ -13,7 +13,7 @@ function buscarTarefaPorId(id){
 
 function listarTarefasPaginadas(page = 0){
     let offset = (page > 1) ? (page - 1) * 10 : 0;
-    return `SELECT t.task_user_id, t.task_id, u.user_fullname, t.task_title, t.task_description, p.priority_description, tp.type_description, s.status_description, t.created_at
+    return `SELECT  t.task_id, t.task_user_id, u.user_fullname, t.task_title, t.task_description, p.priority_description, tp.type_description, s.status_description, t.created_at
     FROM tasks t
         inner join users u ON u.user_id = t.task_user_id
         inner join priorities p ON p.priority_id = t.task_priority_id
